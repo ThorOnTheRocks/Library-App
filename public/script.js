786c5427383c229cb4b2a26f20bcfc958531dd4a
gianluca galota <gianluca.galota@gmail.com>
2021-04-01 16:30:29 +0100

    missing styling and fixing

--- public/script.js
+++ public/script.js
@@ -1,134 +1,153 @@
-// Variables to call Elements 
-const buttonForm = document.querySelector(".btn-form");
-const bookForm = document.querySelector(".book-form");
-const saveNewBook = document.querySelector(".btn-save");
-const booksCard = document.querySelector(".books-card");
-const modal = document.querySelector(".modal");
+// Variables to call Elements by ID.
+const showFormBtn = document.getElementById("showFormBtn"),
+  form = document.getElementById("bookForm"),
+  libraryDivContainer = document.getElementById("renderLibrary"),
+  removeBookBtnQuery = document.getElementsByClassName("deleteBookBtn"),
+  openModalTrigger = document.querySelector(".trigger"),
+  closeModalTrigger = document.querySelector(".close"),
+  modal = document.querySelector(".modal")
 
-
+// Initialize the Library Array and new book variable.
+let myLibrary = []
+let newBook
+let tempLibrary
 
 // Book Constructor:
 function Book(title, author, pages, read = false) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
 }
 
-// UI Tasks
-function displayBooks() {
-  const storedBooks = [
-    {
-      title: "The Hobbit",
-      author: "J.R.R. Tolkien",
-      pages: 670,
-      read: true
-    },
-    {
-      title: "My Brilliant Friend",
-      author: "Elena Ferrante",
-      pages: 436
+// Modal EventListeners for Open, Close, and click outside Modal.
+openModalTrigger.addEventListener("click", () => modal.classList.add("open"))
+closeModalTrigger.addEventListener("click", () => modal.classList.remove("open"))
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.classList.remove("open")
+  }
+})
+
+// Eventlistener to read inputs on form submission.
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  let formValue = event.target.elements
+
+  newBook = new Book(
+    formValue.title.value,
+    formValue.author.value,
+    formValue.pages.value,
+    formValue.read.checked
+  )
+
+  // Pushes Obj to library array.
+  myLibrary.push(newBook)
+  // Renders new book on Submission.
+  renderBook(newBook)
+  // Reset the form fields after submission.
+  form.reset()
+  modal.classList.remove('open')
+  window.localStorage.clear()
+  window.localStorage.setItem("myLibrary", JSON.stringify(myLibrary))
+  return false
+})
+
+const renderBook = (book) => {
+  // Create Buttons to delete book and for read toggle.
+  let removeBookBtn = document.createElement("button")
+  let btnNode = document.createElement("button")
+  removeBookBtn.innerText = "Remove"
+
+  // Create container to separate each book entry.
+  let bookContainerDiv = document.createElement("div")
+
+  // Add a Class for styling in CSS and Eventlistener handling.
+  bookContainerDiv.classList.add("bookContainer")
+  removeBookBtn.classList.add("deleteBookBtn")
+  removeBookBtn.setAttribute("type", "button")
+
+  // Append the Div to the Initial Container.
+  libraryDivContainer.appendChild(bookContainerDiv)
+
+  // Look through the book information entered to display details.
+  for (const [key, value] of Object.entries(book)) {
+    // Create a P tag for every detail and append to page.
+    let pNode = document.createElement("p")
+
+    // Logic for displaying book information to screen.
+    if (key === "pages") {
+      pNode.innerHTML = `Number of Pages: ` + `<span class="numberStyle">` + value + `</span>`
+      pNode.classList.add("numPages")
+    } else if (key === "read") {
+
+      if (value) {
+        btnNode.innerHTML = "Read: Yes"
+        btnNode.style.backgroundColor = "limegreen"
+      } else {
+        btnNode.innerHTML = "Read: No"
+        btnNode.style.backgroundColor = "Red"
+      }
+
+    } else if (key === "author") {
+      pNode.innerHTML = `by ${value}`
+      pNode.classList.add("bookAuthor")
+    } else {
+      pNode.innerHTML = value
+      pNode.classList.add("bookTitle")
     }
-  ];
 
-  const books = storedBooks;
+    pNode.classList.add("bookDetail")
+    btnNode.classList.add("readBtn")
+    btnNode.setAttribute("type", "button")
+    bookContainerDiv.append(pNode)
+    bookContainerDiv.append(btnNode)
+  }
+  bookContainerDiv.appendChild(removeBookBtn)
+
+  // Parse the LocalStorage in preparations to remove any books.
+  let localDB = JSON.parse(localStorage.getItem("myLibrary"))
+
+  removeBookBtn.addEventListener("click", () => {
+    gi
+    // Loop through the myLibrary array until finding book to remove on screen and within the array.
+    myLibrary.map((value, index) => {
+      let authorElement = bookContainerDiv.childNodes[1].innerHTML.slice(3)
+      if (myLibrary[index].author === authorElement) {
+        myLibrary.splice(index, 1)
+        localDB.splice(index, 1)
+        bookContainerDiv.remove()
+        // Stringify the localstorage before readding it.
+        tempLibrary = JSON.stringify(localDB)
+        localStorage.setItem("myLibrary", tempLibrary)
+      }
+
+    })
 
 
-  books.forEach((book) => addBookToLibrary(book));
+  })
+
+  // EventHandler to toggle the read button.
+  btnNode.addEventListener("click", () => {
+    if (btnNode.innerHTML.split(" ")[1] === "Yes") {
+      btnNode.style.backgroundColor = "Red"
+      btnNode.innerText = "Read: No"
+    } else {
+      btnNode.style.backgroundColor = "limegreen"
+      btnNode.innerText = "Read: Yes"
+    }
+  })
 }
 
-
-
-function addBookToLibrary(book) {
-  // Create card element
-  const card = document.createElement('div');
-  card.classList = 'card-body';
-
-  // Construct card content
-  const content = `
-  <div class="col-sm-2 mt-4">
-    <div class="card">
-      <div class="card-header">
-        <h5>${book.title}</h5>
-      </div>
-  
-        <div class="card-body">
-  
-          
-          <p>Author: ${book.author}</p>
-          <p>Pages: ${book.pages}</p>
-          <p>Read: ${book.read}</p>
-        </div>
-  
-        <button class="btn btn-primary delete">Remove</button>
-      
-    </div>
-  </div
-    `;
-
-  // Append newyly created card element to the container
-  booksCard.innerHTML += content;
-}
-
-function deleteBook(book) {
-  if (book.classList.contains("delete")) {
-    book.parentElement.remove();
+// Render myLibrary through Localstorage when page is refreshed.
+function renderLibraryStorage() {
+  if (localStorage.myLibrary) {
+    let getBooks = JSON.parse(localStorage.getItem("myLibrary"))
+    myLibrary = getBooks
+    myLibrary.map((value) => {
+      renderBook(value)
+    })
   }
 }
 
-// function showAlert(message, className) {
-//   const div = document.createElement("div");
-//   div.className = `alert alert-${className}`;
-//   div.appendChild(document.createTextNode(message));
-//   modal.insertBefore(div, bookForm);
-
-
-//   // Vanish in 3 seconds
-//   setTimeout(() => document.querySelector(".alert").remove(), 3000)
-
-
-// }
-
-function clearFormFields() {
-  document.querySelector("#title").value = "";
-  document.querySelector("#author").value = "";
-  document.querySelector("#pages").value = "";
-}
-
-
-
-
-// Event Listeners to display a book
-document.addEventListener("DOMContentLoaded", displayBooks);
-
-// Event add a Book
-bookForm.addEventListener("submit", (e) => {
-
-  // Prevent submit
-  e.preventDefault();
-
-  // Get form values
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const read = document.querySelector(".read").checked;
-
-
-  // Instantiate book
-  const book = new Book(title, author, pages, read);
-
-  // Add book to card
-  addBookToLibrary(book);
-
-
-  // Clear fields form
-  clearFormFields();
-
-});
-
-// Event: Remove book
-booksCard.addEventListener("click", (e) => {
-  deleteBook(e.target)
-
-})
-
+renderLibraryStorage()
